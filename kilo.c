ce8c34be1a56a7d01945e319825978cf1b45d39c
Alexandroskw <filipbilly.120@gmail.com>
2025-04-27 16:34:54 -0600

    Printing the tildes acording to the terminal size

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -10,6 +11,8 @@
 
 // *** data ***
 struct editorConfig {
+        int screenrows;
+        int screencols;
         struct termios orig_termios;
 };
 
@@ -42,10 +45,11 @@ void enableRawMode() {
 
 // *** output ***
 void editorDrawRows() {
-        for (int y = 0; y < 24; y++) {
+        for (int y = 0; y < E.screenrows; y++) {
                 write(STDOUT_FILENO, "~\r\n", 3);
         }
 }
+
 char editorReadKey() {
         int nread;
         char c;
@@ -55,6 +59,18 @@ char editorReadKey() {
         }
 
         return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+        struct winsize ws;
+
+        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) return -1;
+        else {
+                *cols = ws.ws_col;
+                *rows = ws.ws_row;
+
+                return 0;
+        }
 }
 
 void editorRefreshScreen() {
@@ -79,8 +95,13 @@ void editorProcessKeypress() {
 }
 
 // *** Init ***
+void initEditor() {
+        if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
         enableRawMode();
+        initEditor();
 
         char c;
         while (1) {
